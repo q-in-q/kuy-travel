7243e63d96b1a0adedd3a4abeab40245590376aa
q-in-q <52076534+q-in-q@users.noreply.github.com>
2019-06-27 22:34:36 +0700

    Create dropdown.php

---
+++ dropdown.php
@@ -0,0 +1,80 @@
+
+        <div class="dropdown-menu mega-menu v-2 z-depth-1 special-color py-5 px-3"
+          aria-labelledby="navbarDropdownMenuLink3">
+          <div class="row">
+            <div class="col-md-12 col-xl-4 sub-menu mb-xl-0 mb-4">
+              <h6 class="sub-title text-uppercase font-weight-bold white-text">Featured</h6>
+              <!--Featured image-->
+              <a href="#!" class="view overlay z-depth-1 p-0 mb-2">
+                <img src="https://mdbootstrap.com/img/Photos/Horizontal/Work/8-col/img%20(37).jpg"
+                  class="img-fluid" alt="First sample image">
+                <div class="mask rgba-white-slight"></div>
+              </a>
+              <a class="news-title font-weight-bold pl-0" href="#!">Lorem ipsum dolor sit</a>
+              <p class="font-small text-uppercase white-text">
+                <i class="fas fa-clock-o pr-2" aria-hidden="true"></i>July 17, 2017 / <i
+                  class="far fa-comments px-1" aria-hidden="true"></i> 20
+              </p>
+            </div>
+            <div class="col-md-6 col-xl-4 sub-menu mb-md-0 mb-4">
+              <h6 class="sub-title text-uppercase font-weight-bold white-text">Related</h6>
+              <ul class="list-unstyled">
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Quis nostrud exercitation
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Duis aute irure dolor in
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Laboris nisi ut aliquip ex ea commodo consequat
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Reprehenderit in voluptate
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Esse cillum dolore eu fugiat nulla pariatur
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-xl-4 sub-menu mb-0">
+              <h6 class="sub-title text-uppercase font-weight-bold white-text">Design</h6>
+              <ul class="list-unstyled">
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Excepteur sint occaecat
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Sunt in culpa qui officia
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Sed ut perspiciatis unde omnis iste natus error
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Mollit anim id est laborum
+                  </a>
+                </li>
+                <li>
+                  <a class="menu-item pl-0" href="#!">
+                    <i class="fas fa-caret-right pl-1 pr-3"></i>Accusantium doloremque laudantium
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+</div>
