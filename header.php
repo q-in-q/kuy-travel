3976564fb9a0a3cfc37784f96980fabb548b3a23
q-in-q <52076534+q-in-q@users.noreply.github.com>
2019-06-29 11:05:52 +0700

    Update header.php

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <form class="col-md-4 form-inline my-2 my-lg-0">
-        <input class="form-control" type="search" placeholder="Search" data-toggle="modal" data-target="#centralModallg" aria-label="Search">
+        <input class="form-control" type="search" placeholder="Search" data-toggle="modal" data-target="#centralModallg" aria-label="Search" data-target="#centralModalSm">
       </form>
       <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
@@ -108,6 +108,35 @@
         <button type="button" class="btn btn-light">PROFIL</button>
         </li>
       </ul>
+
+      <!--modallllsearchinggg bossskuuu-->
+
+      <!-- Central Modal Small -->
+<div class="modal fade" id="centralModalSm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+
+  <!-- Change class .modal-sm to change the size of the modal -->
+  <div class="modal-dialog modal-sm" role="document">
+
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title w-100" id="myModalLabel">Modal title</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary btn-sm">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Central Modal Small -->
      
 
     </div>
