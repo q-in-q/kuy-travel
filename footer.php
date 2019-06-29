c3962c608bbfa61c519b2a47bc378fb7cc71f71f
q-in-q <52076534+q-in-q@users.noreply.github.com>
2019-06-29 09:55:15 +0700

    a

--- footer.php
+++ footer.php
@@ -1,2 +1,157 @@
-</body>
+ <footer class="position-relative z-index-10 d-print-none" style="padding-top: 80px; padding-bottom: 50px;">
+  <div class="py-6 bg-gray-200 text-muted">
+    <div class="container" style="padding-top: 20px;">
+      <div class="row">
+        <div class="col-lg-4 mb-5 mb-lg-0">
+          <div class="font-weight-bold textt-uppercase text-dark mb-3">
+            Directory
+          </div>
+          <p>lorem ispsum dolor sit amet, consectetur adipisicing</p>
+          <ul class="list-inline">
+            <li class="list-inline-item">
+              <a href="#" target="_blank" title="twitter"
+              class="text-muted text-hover-primary">
+                <i class="fab fa-twitter">
+                </i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" target="_blank" title="facebook"
+              class="text-muted text-hover-primary">
+                <i class="fab fa-facebook-f">
+                </i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" target="_blank" title="instagram"
+              class="text-muted text-hover-primary">
+                <i class="fab fa-instagram">
+                </i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" target="_blank" title="pinterest"
+              class="text-muted text-hover-primary">
+                <i class="fab fa-pinterest">
+                </i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#" target="_blank" title="vimeo"
+              class="text-muted text-hover-primary">
+                <i class="fab fa-vimeo">
+                </i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 mb-5 mb-lg-0">
+          <h6 class="font-weight-bold textt-uppercase text-dark mb-3">
+            Rentals
+          </h6>
+          <ul class="list-unstyled">
+            <li>
+              <a href="#" class="text-dark">
+                Rooms
+              </a>
+            </li>
+            <li>
+              <a href="#" class="text-dark">Map on top</a>
+            </li>
+            <li>
+              <a href="#" class="text-dark">Side Map</a>
+            </li>
+            <li>
+              <a href="#" class="text-dark">No map</a>
+            </li>
+              <li>
+              <a href="#" class="text-dark">Room detail</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 mb-5 mb-lg-0">
+          <h6 class="font-weight-bold textt-uppercase text-dark mb-3">Pages</h6>
+          <ul class="list-unstyled">
+            <li>
+              <a href="Three.html" class="text-dark"
+              data-toggle="tooltip" title="Any Problem?">Contact US</a>
+            </li>
+            <li>
+              <a href="#" class="text-dark">Pricing</a>
+            </li>
+            <li>
+              <a href="#" class="text-dark">Text page</a>
+            </li>
+            <li>
+              <a href="#" class="text-dark">F.A.Q.s
+                <span class="badge badge-info ml-1">New</span></a>
+            </li>
+            <li>
+              <a href="#" class="text-dark">Coming Soon</a>
+            </li>
+          </ul>
+        </div>
+        <script>
+          $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+          });
+        </script>
+        <div class="col-lg-4">
+          <h6 class="font-weight-bold textt-uppercase text-dark mb-3">
+            Daily Offers & Discounts
+          </h6>
+          <p class="mb-3">
+            Lorem ipsum dolor sitamet, consectetur adipisicing elit. At itaque temporibus.
+          </p>
+          <form action="#" id="newsletter-form">
+            <div class="input-group mb-3">
+              <input type="email" placeholder="Your Email Address" 
+              aria-label="Your Email Address" class="form-control 
+              bg-transparent border-dark border-right-0">
+              <div class="input-group-append">
+                <button type="submit" class="btn btn-outline-dark
+                border-left-0">
+                <i class="fas fa-paper-plane text-lg"></i>
+                
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<div style="width: 1318px; height: 90px; background: orange;
+margin-top: 20px; padding-top: 20px;">
+    <div class="container" >
+      <div class="row align-items-center">
+        <div class="col-md-6 text-center text-md-left">
+          <p class="text-sm mb-md-0">Copyright 2019 Your Company. All Rights reserved</p>
+        </div>
+        <div class="col-md-6">
+          <ul class="list-inline-item">
+            <li class="list-inline-item">
+              <img src="https://d19m59y37dris4.cloudfront.net/directory/1-2/img/visa.svg" alt="..."
+              class="w-2rem" style="margin-top:20px; margin-left: 220px; width: 30px; height: 30px;">
+            </li>
+            <li class="list-inline-item">
+              <img src="https://d19m59y37dris4.cloudfront.net/directory/1-2/img/paypal.svg" alt="..."
+              class="w-2rem" style="margin-top: 20px; width: 30px; height: 30px;">
+            </li>
+            <li class="list-inline-item">
+              <img src="https://d19m59y37dris4.cloudfront.net/directory/1-2/img/western-union.svg" alt="..."
+              class="w-2rem" style="width: 30px; height: 30px; margin-top: 20px;">
+            </li>
+            <li class="list-inline-item">
+              <img src="https://d19m59y37dris4.cloudfront.net/directory/1-2/img/mastercard.svg" alt="..."
+              class="w-2rem" style="margin-top:20px; width: 30px; height: 30px;">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+ </body>
 </html>
