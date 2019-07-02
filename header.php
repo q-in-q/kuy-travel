ee7d7db9d9bcedb61792f202c7beb52bf47f7c05
q-in-q <52076534+q-in-q@users.noreply.github.com>
2019-07-02 19:25:35 +0700

    update promo bsquuuu

--- header.php
+++ header.php
@@ -10,12 +10,16 @@
     <script src="js/popper.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js" type="text/javascript"></script>
     <script src="js/jquery.js" type="text/javascript"></script>
-    <script src="js/slider.js" type="text.javascript"></script>
+    <script src="js/slider.js" type="text/javascript"></script>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="fontawesome-free-5.9.0-web/css/all.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/stylecountdown.css">
+
+    
+    <script src="js/countdown.js" type="text/javascript"></script>
 
     <!-- Lnk script -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -27,6 +31,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+   
+
+    <!--countdown-->
+    
     <title>Travel jogja</title>
     
 </head>
@@ -61,7 +69,7 @@
            <a class="nav-link" href="#">Booking</a>
          </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Discount</a>
+            <a class="nav-link" href="#" onclick="window.location.href='promo.php'">Discount</a>
           </li>
         
           
---
+++ promo.php
@@ -0,0 +1,133 @@
+<?php include("header.php");?>
+<hr>
+<br><br>
+<!-- destinasi -->
+<div class="home">
+        <h2>DISCOUNT</h2>
+        <div class="underline"></div>
+    </div>
+
+    <div  class="container-fluid">
+
+        <div class="row">
+
+        
+
+            <div class="destinasi col-md-4 ">
+              
+                <div class="produk1">
+                    <div class="zoom-effect">
+                      <div class="kotak">
+                        <img src="image/timang.jpg" />
+                      </div>
+                      <div class=fullwidth>
+
+                      </div>
+
+                      <div class="carousel-caption">
+                        <h1>Discount 1 day in Pantai Timang</h1>
+                        <div class="container text-transparent">
+                            <ul class="countdown">
+                                
+                                <h4 >Berlaku hingga</h4>
+                                <li><span id="days"></span>days</li>
+                                <li><span id="hours"></span>Hours</li>
+                                <li><span id="minutes"></span>Minutes</li>
+                                <li><span id="seconds"></span>Seconds</li>
+                            </ul>
+                         </div>
+                         <button type="button" class="btn btn-primary">Detail</button><br>
+                      </div>
+                    </div>
+                   
+              </div>
+
+            </div>
+
+            <div class="destinasi col-md-4 ">
+              
+              <div class="produk1">
+                    <div class="zoom-effect">
+                      <div class="kotak">
+                        <img src="image/nglambor.jpg" />
+                      </div>
+
+                      <div class="carousel-caption">
+                        <h1>Discount 50% Full Service in Pantai Nglombor</h1>
+                        <div class="container text-transparent">
+                            <ul class="countdown">
+                                
+                                <h4 class="berakhir">Promo berakhir</h4>
+                                <li><span id="days"></span>days</li>
+                                <li><span id="hours"></span>Hours</li>
+                                <li><span id="minutes"></span>Minutes</li>
+                                <li><span id="seconds"></span>Seconds</li>
+                            </ul>
+                         </div>
+                        <button type="button" class="btn btn-primary">Detail</button>
+                      </div>
+                    </div>
+              </div>
+
+          </div>
+
+          <div class="destinasi col-md-4 ">
+              
+              <div class="produk1">
+                    <div class="zoom-effect">
+                      <div class="kotak">
+                        <img src="image/pindul.jpg" />
+                      </div>
+
+                      <div class="carousel-caption">
+                        <h1>Free Tour Guide in Goa Pindul</h1>
+                        <div class="container text-transparent">
+                            <ul class="countdown">
+                                
+                                <h4 class="berakhir">Promo berakhir</h4>
+                                <li><span id="days"></span>days</li>
+                                <li><span id="hours"></span>Hours</li>
+                                <li><span id="minutes"></span>Minutes</li>
+                                <li><span id="seconds"></span>Seconds</li>
+                            </ul>
+                         </div>
+                        <button type="button" class="btn btn-primary">Detail</button>
+                      </div>
+                    </div>
+              </div>
+
+          </div>
+
+
+
+              <div class="clear"></div>
+
+              <div class="row text-center">
+                  <button type="button" class="btn btn-primary" > <a onclick="window.location.href='promoall.php'">View All</a></button>
+
+              </div>
+
+            
+
+
+        </div>
+
+    </div>
+
+   
+
+</div> <!--tutup-->
+
+<script>
+const myCountdown = new countdown({
+    target: '.countdown',
+    dayWord: ' days',
+    hourWord: ' hours',
+    minWord: ' mins',
+    secWord: ' seconds'
+  });
+</script>
+
+
+
+<?php include("footer.php");?>
