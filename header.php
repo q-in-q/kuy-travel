f1d807f6706385e4463b3ec0212452213ce253c7
q-in-q <52076534+q-in-q@users.noreply.github.com>
2019-07-06 11:36:41 +0700

    s

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 
 <nav class="kepala navbar navbar-expand-lg navbar-light fixed-top" style="background:#343a40;">
 
-    <a class="navbar-brand" href="#" style="color:#ffffff">KuyTravel</a>
+    <a class="navbar-brand" href="#" style="color:#ffffff" onclick="window.location.href='index.php'">KuyTravel</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
