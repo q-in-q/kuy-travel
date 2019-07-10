a66b083c9de6ad5a8d26e7001e006b48965a1aa7
q-in-q <52076534+q-in-q@users.noreply.github.com>
2019-07-10 20:41:12 +0700

    Beranda selesai

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
   <link rel="stylesheet" href="fontawesome/css/all.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,600,600i,700,800,900|Poiret+One&display=swap" rel="stylesheet">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
--- ourteam.php
+++ ourteam.php
@@ -1,54 +1,20 @@
 <?php include("header.php"); ?>
 
-<!--Carousel Wrapper-->
-<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
-  <!--Indicators-->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-1z" data-slide-to="1"></li>
-    <li data-target="#carousel-example-1z" data-slide-to="2"></li>
-  </ol>
-  <!--/.Indicators-->
-  <!--Slides-->
-  <div class="carousel-inner" role="listbox">
-    <!--First slide-->
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
-        alt="First slide">
-    </div>
-    <!--/First slide-->
-    <!--Second slide-->
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
-        alt="Second slide">
-    </div>
-    <!--/Second slide-->
-    <!--Third slide-->
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
-        alt="Third slide">
-    </div>
-    <!--/Third slide-->
-  </div>
-  <!--/.Slides-->
-  <!--Controls-->
-  <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  <!--/.Controls-->
-</div>
-<!--/.Carousel Wrapper-->
 
+<style>
+  body  {
+    font-family: 'Monsstreat',sans-serif;
+  }
+
+</style>
+<br>
+<br>
+<br>
 <!--bagian isi--->
 <!-- travel service -->
     <div class="container-fluid menu-utama">
       <div class="atas">
-          <h1>TREVEL SERVICE</h1>
+          <h1>TRAVEL SERVICE</h1>
           <h6>Travel kami memberikan pelayanan yang nyaman bagi anda dan memudahkan para pengguna, sebagai berikut:<h6>
       </div>
       <div class="container list">
@@ -125,7 +91,7 @@
                         <div class="container col-5 card">
                           <div class="col-lg-6">
                             <div class="container foto" style="height:80%">
-                              <img src="./image/rofiqah.jpg" alt="" />
+                              <img src="17_11_1168.jpg" alt="" />
                             </div>
                           </div>
                           <div class="col-lg-11">
